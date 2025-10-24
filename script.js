e4d69ce44864320095b439d5f1eebb0b7cfc8740
charlbkg <156369623+charlbkg@users.noreply.github.com>
2025-10-23 20:19:52 -0400

    First try for browser interoperability

--- script.js
+++ script.js
@@ -1,133 +1,87 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2hhcmxvdHRlYmtnIiwiYSI6ImNscjlvYzM1OTA1MW8ya24xbjdmNHRhaWkifQ.H2WW8WJZiHWFksxymyigTw'; //Add default public map token from your Mapbox account
 const map = new mapboxgl.Map({
  container: 'my-map', // map container ID
- style: 'mapbox://styles/charlottebkg/clsexueex038t01o8cshrhdka', // style URL
- center: [-79.400831, 43.659723], // starting position [lng, lat]
- zoom: 15, // starting zoom
+ style: 'mapbox://styles/charlottebkg/cltadlaho00nt01qkgei36fnn', // style URL
+center: [-71.09647636158911,
+          42.386864529111335], // starting position [lng, lat]
+zoom: 17, // starting zoom
 });
+// Add zoom and rotation controls, position controls
+map.addControl(new mapboxgl.NavigationControl(), 'bottom-left');
+map.addControl(new mapboxgl.FullscreenControl(), 'bottom-left');
+//Adding data for pop-ups
 map.on('load', () => {
-    //Add a data source containing GeoJSON data
-    map.addSource('experience-trajectory', {
-    type: 'geojson',
-    data: {
-    "type": "FeatureCollection",
-    "features": [
-    {
-        "type": "Feature",
-        "properties": {},
-        "geometry": {
-          "coordinates": [
-            [
-              -79.41100385187077,
-              43.651392223722894
-            ],
-            [
-              -79.392242927534,
-              43.67004272013182
-            ],
-            [
-              -79.39856964482043,
-              43.662406209739885
-            ],
-            [
-              -79.39871677778092,
-              43.663257715465534
-            ],
-            [
-              -79.41302545815543,
-              43.664721212714
-            ],
-            [
-              -79.40073985598264,
-              43.65963873272145
-            ],
-            [
-              -79.39816502917994,
-              43.65870734157346
-            ],
-            [
-              -79.39875356102075,
-              43.66237959999151
-            ],
-            [
-               -79.39508797629006,
-            43.66177701729015
-              ]
-            
-          ],
-          "type": "LineString"
-        }
+    map.addSource('places', {
+        'type': 'geojson',
+        'data': 'https://raw.githubusercontent.com/charlbkg/GGR472Lab3/main/Resume.geojson'
     }
-    ]
-    }
-    });
+    );
+    // Add a layer showing the places.
+    // I wanted to keep the symbology from my mapbox style containing a tileset of the resume geoJSON,
+    //but still add the popup functionality. So I loaded the same data for the tileset at a geoJSON and 
+    //made the points transparent. This way the symbology of the style shows, and the pop-up still works.
     map.addLayer({
-        'id': 'route',
-        'type': 'line',
-        'source': 'experience-trajectory',
-        'layout': {
-        'line-join': 'round',
-        'line-cap': 'round'
-        },
+        'id': 'places-points',
+        'type': 'circle',
+        'source': 'places',
         'paint': {
-        'line-color': '#888',
-        'line-width': 8
+            'circle-color': 'transparent',
+            'circle-radius': 20,
+            'circle-stroke-width': 2,
+            'circle-stroke-color': 'transparent'
         }
-        });
-        // add a line layer with line-dasharray set to the first value in dashArraySequence
-map.addLayer({
-    type: 'line',
-    source: 'experience-trajectory',
-    id: 'line-dashed',
-    paint: {
-    'line-color': 'yellow',
-    'line-width': 6,
-    'line-dasharray': [0, 4, 3]
-    }
     });
-     
-    // technique based on https://jsfiddle.net/2mws8y3q/
-    // an array of valid line-dasharray values, specifying the lengths of the alternating dashes and gaps that form the dash pattern
-    const dashArraySequence = [
-    [0, 4, 3],
-    [0.5, 4, 2.5],
-    [1, 4, 2],
-    [1.5, 4, 1.5],
-    [2, 4, 1],
-    [2.5, 4, 0.5],
-    [3, 4, 0],
-    [0, 0.5, 3, 3.5],
-    [0, 1, 3, 3],
-    [0, 1.5, 3, 2.5],
-    [0, 2, 3, 2],
-    [0, 2.5, 3, 1.5],
-    [0, 3, 3, 1],
-    [0, 3.5, 3, 0.5]
-    ];
-     
-    let step = 0;
-     
-    function animateDashArray(timestamp) {
-    // Update line-dasharray using the next value in dashArraySequence. The
-    // divisor in the expression `timestamp / 50` controls the animation speed.
-    const newStep = parseInt(
-    (timestamp / 50) % dashArraySequence.length
-    );
-     
-    if (newStep !== step) {
-    map.setPaintProperty(
-    'line-dashed',
-    'line-dasharray',
-    dashArraySequence[step]
-    );
-    step = newStep;
+});
+// Create a popup, but don't add it to the map yet.
+const popup = new mapboxgl.Popup({
+    closeButton: false,
+    closeOnClick: false
+});
+map.on('mouseenter', 'places-points', (e) => {
+    map.getCanvas().style.cursor = 'pointer';
+
+    // Copy coordinates array.
+    const coordinates = e.features[0].geometry.coordinates.slice();
+    const description = e.features[0].properties.Description;
+    console.log(coordinates)
+    // Ensure that if the map is zoomed out such that multiple
+    // copies of the feature are visible, the popup appears
+    // over the copy being pointed to.
+    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
     }
-     
-    // Request the next frame of the animation.
-    requestAnimationFrame(animateDashArray);
-    }
-     
-    // start the animation
-    animateDashArray(0);
+    // Populate the popup and set its coordinates
+    // based on the feature found.
+    popup.setLngLat(coordinates).setHTML(description).addTo(map);
+});
+
+map.on('mouseleave', 'places-points', () => {
+    map.getCanvas().style.cursor = '';
+    popup.remove();
+});
+
+//Create event listener to fly to location when the button is clicked, and function that cycles through each point
+document.getElementById('About us').addEventListener('click', function () {
+    // Back to the first coordinate.
+    map.flyTo({
+        zoom: 17,
+        center: [-72.09647636158911,
+          45.386864529111335],
     });
-   
+});
+
+// fetch('Resume.geojson')
+//       .then(response => response.json())
+//       .then(data => createList(data));
+
+// const data = JSON.parse(jsonData);
+//     const listContainer = document.getElementById('listContainer');
+//     const ul = document.createElement('ul'); // Create an unordered list
+
+//     data.forEach(item => {
+//         const li = document.createElement('li'); // Create a list item
+//         li.textContent = item.ExperienceType; // Set the text content from the JSON data
+//         ul.appendChild(li); // Append the list item to the unordered list
+//     });
+
+//     listContainer.appendChild(ul); // Append the unordered list to the container
